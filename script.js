6df3480635e76627eba8ca6a0b3d11ef5d18aaac
MUTHUKRISHNAN S <77878932+MuthuKrishnan27@users.noreply.github.com>
2023-09-09 11:48:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -7,19 +7,29 @@ let priceValidation = Number(0);
 
 let total = document.getElementById("total");
 
+let itemQty =document.getElementById("item-qty-input");
 
+let num = Number(0);
 
 form.addEventListener("submit",(e)=>{
 	e.preventDefault();
 	let inputValue = document.getElementById("item-name-input").value;
 	let priceValue = document.getElementById("item-price-input").value;
+	num = Number(num+1);
 	priceValidation+=Number(priceValue);
+	
+	let sl = document.createElement("div");
+	sl.innerText = num;
+	itemQty.appendChild(sl);
+	
 	let item = document.createElement("div");
 	item.innerText = inputValue;
 	itemColumn.appendChild(item);
+	
 	let price = document.createElement("div");
 	price.innerText = priceValue;
 	priceColumn.appendChild(price);
+	
 	total.innerText=`Total = ${priceValidation}`;
 	form.reset();
 });
